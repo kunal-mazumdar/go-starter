b93284f868599bd29aa880ca775ca34b406016a8
Kunal Mazumdar <kunalm@jfrog.com>
2023-12-07 08:52:41 +0530

    Change

--- main.go
+++ main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"fmt"
+	"github.com/otiai10/copy"
 )
 
 func main() {
+	err := copy.Copy("/tmp/a.txt", "/tmp/b.txt")
+	if err != nil {
+		fmt.Print(err)
+	}
 	fmt.Print("Test 1")
 }
